1c516955e48148dd744d2940d85da7c38db53fe2
Joshua Mo <joshua.mo.876@gmail.com>
2024-03-30 12:16:47 +0000

    refactor: use Args.parse

--- csv2json/bin/main.ml
+++ csv2json/bin/main.ml
@@ -1,43 +1,48 @@
-type exe = { id : int; message : string } [@@deriving yojson]
+type my_json_record = { id : int; message : string } [@@deriving yojson]
 
-exception InvalidArgument of string;;
+exception InvalidArgument of string
 
-if Array.length Sys.argv < 2 then raise (InvalidArgument "Usage: <input-file>")
-
-let input_filename = Sys.argv.(1)
-
-let output_filename =
-  if Array.length Sys.argv = 3 then Sys.argv.(2) else "meme.json"
-;;
-
-let is_equal = Filename.check_suffix input_filename ".csv" in
-match is_equal with
-| false -> raise (InvalidArgument "That's not a CSV file!")
-| true -> ()
-
-let () = Printf.printf "Converting %s to JSON...\n" input_filename
+let input_filename = ref ""
+let output_filename = ref ""
+let anon_fun filename = input_filename := filename
+let speclist = [ ("-o", Arg.Set_string output_filename, "Output filename") ]
+let usage_msg = "Usage: <input-file> -o <output-file>"
+let () = Arg.parse speclist anon_fun usage_msg
 
 let () =
-  let oc = open_in input_filename in
-  let lines = In_channel.input_lines oc in
-  let make_row csvrow =
-    List.map
-      (fun row ->
-        let exerow = String.split_on_char ',' row in
-        let res =
-          {
-            id = int_of_string (List.nth exerow 0);
-            message = List.nth exerow 1;
-          }
-        in
-        exe_to_yojson res)
-      csvrow
-  in
-  let t = make_row lines in
+  match !output_filename with
+  | "" -> output_filename := "messages.json"
+  | _ -> ()
 
-  List.iter
-    (fun row ->
-      Yojson.Safe.pretty_to_channel
-        (open_out_gen [ Open_append; Open_creat ] 0o777 output_filename)
-        row)
-    t
+let () =
+  match Filename.check_suffix !output_filename ".json" with
+  | false -> raise (InvalidArgument "That's not a JSON file!")
+  | true -> ()
+
+let () =
+  let oc = open_in !input_filename in
+  let lines = In_channel.input_lines oc in
+  match lines with
+  | [] -> ()
+  | _ :: t ->
+      let make_row csvrow =
+        List.map
+          (fun row ->
+            let exerow = String.split_on_char ',' row in
+            let res =
+              {
+                id = int_of_string (List.nth exerow 0);
+                message = List.nth exerow 1;
+              }
+            in
+            my_json_record_to_yojson res)
+          csvrow
+      in
+      let rows = make_row t in
+
+      List.iter
+        (fun row ->
+          Yojson.Safe.pretty_to_channel
+            (open_out_gen [ Open_append; Open_creat ] 0o777 !output_filename)
+            row)
+        rows
