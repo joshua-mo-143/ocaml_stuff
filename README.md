e8c363c4ced07cebbd6ff12e46e60abcfdf39821
Joshua Mo <joshua.mo.876@gmail.com>
2024-03-31 13:21:53 +0100

    feat: fix json

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A series of repositories documenting my learnings and experiments with Ocaml.
 
 ## Projects 
 - csv2json: A CLI tool that turns a CSV file into JSON.
+- dream_service: A Dream web service.
--- dream_service/bin/router.ml
+++ dream_service/bin/router.ml
@@ -3,10 +3,9 @@ open Types_j
 let hello_world _req = (Dream.respond "Hello world!")
 
 let return_json _req = (
-  let json = [string_of_meme { id = 1; message = "Hello world from Shuttle!" };
-   string_of_meme { id = 2; message = "Hello world!" }] in
-  let concatted_string = String.concat "," json in
-    Printf.sprintf "[%s]" concatted_string
+  let memes = [ { id = 1; message = "Hello world from Shuttle!" };
+    { id = 2; message = "Hello world!" }] in
+    string_of_memes { title = "My List"; memes = memes }
     |> Dream.json
 
 )
